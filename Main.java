e872375cf7e40332d0f7ddf0025c8565d09b1323
SnedanD <dabresnedan10@gmail.com>
2024-09-15 16:29:23 +0530

    First Commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+// print array elements 
+
+class Main {
+  public static void main(String[] args) {
+      
+    // create an array
+    int[] numbers = {3, 9, 5, -5};
+    
+    // for each loop 
+    for (int number: numbers) {
+      System.out.println(number);
+    }
+  }
+}
---
+++ Stri.java
@@ -0,0 +1,14 @@
+class Stri {
+  public static void main(String[] args) {
+
+    // create a string
+    String a = "Hello! ";
+    String b = "World!";
+    System.out.println("Stringa: " + a);
+    System.out.println("Stringb: " + b);
+
+    // get the length of greet
+    String c = a + b;
+    System.out.println("Length: " + c);
+  }
+}
